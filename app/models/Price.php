41ce43f886a5d4d407bf7afb3912141efbcec23d
jules21 <julesfabien96@gmail.com>
2020-05-11 16:06:57 +0200

    feat(Models): create models - create migration and models - include relationship in some tables
    [Models #0012]

---
+++ app/models/Price.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Price extends Model
+{
+    protected $guarded = [''];
+
+    public function product()
+    {
+        $this->belongsTo('App/Models/Product');
+    }
+}
---
+++ database/migrations/2020_05_11_130530_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('category_id');
+            $table->string('unity_measure');
+            $table->string('quantity_per_unit');
+            $table->string('details');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
