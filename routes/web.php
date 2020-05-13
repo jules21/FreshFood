93fb3519ae964c3fc39845ae88e17655a5b69582
jules fabien <37208871+jules21@users.noreply.github.com>
2020-05-13 19:36:04 +0200

    Revert "Upload Product Images"

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,8 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
- */
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard', function () {
-    return view('layouts.master');
-});
-Route::resource('/products', 'ProductController');
-Route::resource('/categories', 'CategoryController');
-Route::resource('/measures', 'MeasureController');
-
-Route::resource('/products/{product}/uploads', 'UploadController');
